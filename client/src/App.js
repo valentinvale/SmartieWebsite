9836145e9a39b03abe5fd776476dd0faf4b33422
valentinvale <92953402+valentinvale@users.noreply.github.com>
2025-03-07 04:07:10 +0200

    customized style on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './styles/Variables.css';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,28 +1,30 @@
 import React from "react";
 
-import blobs from "../resources/blobs.png";
+import erasmus from "../resources/Erasmus.png";
+
+import "../styles/Home.css";
 
 const HomeComponent = () => {
     return (
-        <div className="homa-page-content">
-            <div style={{display: "flex", flexDirection: "row"}}>
-                <div style={{width: "852px", height: "442px"}}>
+        <div className="home-page-content">
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <div className="text-div" style={{width: "852px", height: "442px"}}>
                     <h1>SMARTIE</h1>
                     <h4>Synergistic Management and Advancement of Artificial Intelligence in European Higher Education</h4>
-                    <p style={{fontSize: "24px"}}>
+                    <p className="home-p" style={{fontSize: "24px"}}>
                         SMARTIE is a collaborative initiative designed to enhance the integration, development, 
                         and responsible use of Artificial Intelligence (AI) within European higher education institutions. 
                         By fostering innovation and strategic AI deployment, SMARTIE aims to equip universities, educators, 
                         and students with the tools, knowledge, and infrastructure necessary to thrive in an increasingly AI-driven world.
                     </p>
                 </div>
-                {/* <div>
+                <div>
                     <img 
-                        src={blobs} 
+                        src={erasmus} 
                         alt="blobs" 
-                        style={{width: "972.41px", height: "818px"}}
+                        style={{width: "590px", height: "56px"}}
                     />
-                </div> */}
+                </div>
             </div>
         </div>
     );
